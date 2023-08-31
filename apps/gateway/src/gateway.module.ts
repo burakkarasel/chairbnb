54286b11ffc567c7a66b8208f5241b8dcd8a2290
Burak Can Karasel <99825584+burakkarasel@users.noreply.github.com>
2023-08-31 22:08:24 +0300

    Set graphql apollo gateway

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { LoggerModule } from "@app/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from "@nestjs/apollo";
+import { IntrospectAndCompose } from "@apollo/gateway";
 
 @Module({
-  imports: [],
-  controllers: [GatewayController],
-  providers: [GatewayService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    LoggerModule,
+    GraphQLModule.forRootAsync<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      useFactory: (configService: ConfigService) => ({
+        gateway: {
+          supergraphSdl: new IntrospectAndCompose({
+            subgraphs: [
+              {
+                name: "reservations",
+                url: configService.getOrThrow("RESERVATIONS_GRAPHQL_URL"),
+              },
+            ],
+          }),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class GatewayModule {}
